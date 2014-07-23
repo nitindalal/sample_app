44318fce56a1e520de272746ba1fccf02867710e
Nitin <nitin@hikeezee.com>
2014-07-23 12:31:24 +0530

    fixed something

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:edit, :update]	
+	before_filter :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -13,9 +15,30 @@ class UsersController < ApplicationController
     if @user.save
     	sign_in @user
     	flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      	redirect_to @user
     else
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 end
