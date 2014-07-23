b078ffd1573ffa9c02734fad0a51a6db19429d0b
Nitin <nitin@hikeezee.com>
2014-07-23 15:05:33 +0530

    finished 9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,4 +27,13 @@ def sign_in(user)
     user == current_user
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
 end
